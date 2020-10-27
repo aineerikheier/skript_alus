37c970130547c04a210143b66eb5049de4c30edd
Ain-Eerik Heier <ain-eerik.heier@khk.ee>
2020-10-27 21:49:32 +0200

    tekstiskript.sh - proovisin siin mõnda lihstamat skripti

---
+++ tekstiskript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#echo "Hetkel asud kataloogis:"
+#pwd
+
+#echo "Oled sisse loginud kasutajana:"
+#whoami
+
+#echo "Praeguse kasutaja kodukaust on: $HOME"
+
+hinne=5
+
+nimi="Taat"
+
+#echo "$nimi sai eksami hindeks $hinne"
+
+sisu=$(ls)
+
+#echo $sisu
+
+nr1=$((10+12))
+
+#echo $nr1
+
+nr2=$(($nr1 - 8))
+
+#echo $nr2
